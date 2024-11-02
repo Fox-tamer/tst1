bbc9c75b86c78e3bdb2cd19a40b3c93d107f756e
Fox-tamer <alaaud1007@gmail.com>
2024-11-03 00:37:42 +0200

    Edited on sunday 021124

--- Hello.cpp
+++ Hello.cpp
@@ -6,6 +6,6 @@ int main()
 {
     int x=6;
     int y=5;
-    cout << "Hello world alaauddin!  " << x*y <<" "<<endl;
+    cout << "Hello world alaauddin El-Safty!  " << x*y <<" "<<endl;
     return 0;
 }
